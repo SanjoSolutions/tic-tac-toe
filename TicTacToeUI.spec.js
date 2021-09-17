7b896ada0702e0b3533d2aaa4b29938b400480ef
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2021-09-17 23:02:54 +0200

    Removes Playwright

--- TicTacToeUI.spec.js
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test'
-
-test('renders an empty grid', async ({ page }) => {
-  await page.goto('http://localhost:8000')
-  const $grid = page.locator('.grid').first()
-  await expect($grid).toBeVisible()
-})
